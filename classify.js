238953633dc62e0760b45ecbbe44d07d40b911bd
Jeff Lu <ljeff@us.ibm.com>
2021-06-06 18:13:58 -0400

    load trained saved model

--- classify.js
+++ classify.js
@@ -1,5 +1,7 @@
 // const tf = require('@tensorflow/tfjs');
 const tfnode = require('@tensorflow/tfjs-node');
+const labels = require('./inference_graph/saved_model/assets/labels.json');
+
 const mobilenet = require('@tensorflow-models/mobilenet');
 const fs = require('fs');
 
@@ -7,7 +9,7 @@ const loadModel = async (modelPath) => {
   // const handler = tfnode.io.fileSystem(`./${modelPath}`);
   // const model = await tfnode.loadLayersModel(handler);
   // const model = await tfnode.loadLayersModel(modelPath);
-  const model = await tfnode.node.loadSavedModel(modelPath, ['serve'], 'serving_default');
+  const model = await tfnode.node.loadSavedModel(modelPath);
   // const model = await tfnode.loadGraphModel(modelPath);
   return model;
 }
@@ -15,15 +17,39 @@ const loadModel = async (modelPath) => {
 const classify = async (imagePath, modelPath) => {
   const image = fs.readFileSync(imagePath);
   const decodedImage = tfnode.node.decodeImage(new Uint8Array(image), 3);
+  const input = decodedImage.expandDims(0);
 
   let model;
   let predictions;
   if(modelPath) {
-    // const modelInfo = await tfnode.node.getMetaGraphsFromSavedModel(modelPath);
-    // console.dir(modelInfo);
+    const modelInfo = await tfnode.node.getMetaGraphsFromSavedModel(modelPath);
+    console.dir(modelInfo);
+    console.dir(modelInfo[0].signatureDefs.serving_default);
     // console.dir(modelInfo[0].signatureDefs.serving_default.outputs, { depth: null, maxArrayLength: null });
     model = await loadModel(modelPath);
-    predictions = model.predict(decodedImage);
+    let outputTensor = await model.predict({input_tensor: input});
+    // console.log('output tensor', outputTensor);
+    const scores = await outputTensor['detection_scores'].arraySync();
+    const boxes = await outputTensor['detection_boxes'].arraySync();
+    const classes = await outputTensor['detection_classes'].arraySync();
+    const num = await outputTensor['num_detections'].arraySync();
+    outputTensor['detection_scores'].dispose();
+    outputTensor['detection_boxes'].dispose();
+    outputTensor['detection_classes'].dispose();
+    outputTensor['num_detections'].dispose();
+    const detectedBoxes = [];
+    const detectedNames = [];
+    const detectedScores = [];
+    for (let i = 0; i < scores[0].length; i++) {
+      if (scores[0][i] > 0.3) {
+        detectedBoxes.push(boxes[0][i]);
+        detectedNames.push(labels[classes[0][i]]);
+        detectedScores.push(scores[0][i]);
+      }
+    }
+    console.log('predictions:', detectedBoxes, detectedNames, detectedScores);
+
+    // predictions = model.predict(decodedImage);
   } else {
     model = await mobilenet.load();
     predictions = await model.classify(decodedImage);
--- webcam.js
+++ webcam.js
@@ -1,8 +1,10 @@
 // const tf = require('@tensorflow/tfjs');
 const tfnode = require('@tensorflow/tfjs-node');
-const mobilenet = require('@tensorflow-models/mobilenet');
+const labels = require('./inference_graph/saved_model/assets/labels.json');
+// const mobilenet = require('@tensorflow-models/mobilenet');
 const fs = require('fs');
 const NodeWebcam = require('node-webcam');
+const Jimp = require('Jimp');
 
 const loadModel = async (modelPath) => {
   // const handler = tfnode.io.fileSystem(`./${modelPath}`);
@@ -11,6 +13,36 @@ const loadModel = async (modelPath) => {
   const model = await tfnode.node.loadSavedModel(modelPath, ['serve'], 'serving_default');
   // const model = await tfnode.loadGraphModel(modelPath);
   return model;
+}
+
+const loadImage = async (buffer) => {
+  let img_tensor;
+  new Jimp({ data: buffer, width: 1280, height: 768 }, (err, image) => {
+    // this image is 1280 x 768, pixels are loaded from the given buffer.
+    // const image = await Jimp.read(buffer);
+    image.cover(224, 224, Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE);
+  
+    const NUM_OF_CHANNELS = 3;
+    let values = new Float32Array(224 * 224 * NUM_OF_CHANNELS);
+  
+    let i = 0;
+    image.scan(0, 0, image.bitmap.width, image.bitmap.height, (x, y, idx) => {
+      const pixel = Jimp.intToRGBA(image.getPixelColor(x, y));
+      pixel.r = pixel.r / 127.0 - 1;
+      pixel.g = pixel.g / 127.0 - 1;
+      pixel.b = pixel.b / 127.0 - 1;
+      pixel.a = pixel.a / 127.0 - 1;
+      values[i * NUM_OF_CHANNELS + 0] = pixel.r;
+      values[i * NUM_OF_CHANNELS + 1] = pixel.g;
+      values[i * NUM_OF_CHANNELS + 2] = pixel.b;
+      i++;
+    });
+  
+    const outShape = [224, 224, NUM_OF_CHANNELS];
+    img_tensor = tf.tensor3d(values, outShape, 'float32');
+    img_tensor = img_tensor.expandDims(0);
+    return img_tensor;  
+  });
 }
 
 //Default options
@@ -59,7 +91,10 @@ const opts = {
 };
 
 const classify = async (modelPath) => {
-  const model = await loadModel(modelPath);
+  const modelInfo = await tfnode.node.getMetaGraphsFromSavedModel(modelPath);
+  console.dir(modelInfo);
+  console.dir(modelInfo[0].signatureDefs.serving_default);
+const model = await loadModel(modelPath);
   const webcam = NodeWebcam.create( opts );
   
   // webcam.getLastShot( function(err, shot) {
@@ -69,14 +104,35 @@ const classify = async (modelPath) => {
   //   });
   // });
 
-  webcam.capture( 'my_picture', ( err, data ) => {
+  webcam.capture( 'my_picture', async ( err, data ) => {
     if(!err) {
       // const image = fs.readFileSync('my_picture.jpg');
       // const decodedImage = tfnode.node.decodeImage(new Uint8Array(image), 3);
       const decodedImage = tfnode.node.decodeImage(new Uint8Array(data), 3);
-      const predictions = model.predict(decodedImage);
-      console.log('predictions:', predictions);
-      // webcam.getBase64(0, (err, data) => {
+      const input = decodedImage.expandDims(0);
+      // let input = tfnode.zeros([1, 224, 224, 3]);
+      // input[0] = decodedImage;
+      let outputTensor = await model.predict({input_tensor: input});
+      const scores = await outputTensor['detection_scores'].arraySync();
+      const boxes = await outputTensor['detection_boxes'].arraySync();
+      const classes = await outputTensor['detection_classes'].arraySync();
+      const num = await outputTensor['num_detections'].arraySync();
+      outputTensor['detection_scores'].dispose();
+      outputTensor['detection_boxes'].dispose();
+      outputTensor['detection_classes'].dispose();
+      outputTensor['num_detections'].dispose();
+      const detectedBoxes = [];
+      const detectedNames = [];
+      const detectedScores = [];
+      for (let i = 0; i < scores[0].length; i++) {
+        if (scores[0][i] > 0.3) {
+          detectedBoxes.push(boxes[0][i]);
+          detectedNames.push(labels[classes[0][i]]);
+          detectedScores.push(scores[0][i]);
+        }
+      }
+      console.log('predictions:', detectedBoxes, detectedNames, detectedScores);
+        // webcam.getBase64(0, (err, data) => {
       //   const predictions = model.predict(data);
       //   console.log('predictions:', predictions);  
       // })
